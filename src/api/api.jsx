836bd91f89102eed8fafbeda1d73609fc2be17fa
leeCH <chan0421@gmail.com>
2024-05-08 15:39:24 +0900

    api업로드

---
+++ src/api/api.jsx
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMu9AQIeRQZMVTKbxfoc1zqeeTfzQeWfU",
+    authDomain: "shop-ch-3c193.firebaseapp.com",
+    projectId: "shop-ch-3c193",
+    storageBucket: "shop-ch-3c193.appspot.com",
+    messagingSenderId: "162184911610",
+    appId: "1:162184911610:web:102d13f027934c10a10743"
+  };
